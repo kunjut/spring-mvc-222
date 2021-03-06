9dbe1fd0237fb12e1cfd9d94f85b078046e8cd5e
kunjut <v.dimnich@gmail.com>
2021-03-06 17:55:52 +0300

    add CarsController

---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,22 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarsController {
+
+    @GetMapping(value = "/cars")
+    public String printWelcome2(ModelMap model) {
+        List<String> messages = new ArrayList<>();
+        messages.add("Hello2!");
+        messages.add("I'm Spring MVC application");
+        messages.add("5.2.0 version by sep'19 ");
+        model.addAttribute("messages", messages);
+        return "index";
+    }
+}
