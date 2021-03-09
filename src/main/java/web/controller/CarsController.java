0ed9a23406c540cb65e63cd28ebd9779f7c081d3
kunjut <v.dimnich@gmail.com>
2021-03-09 12:28:58 +0300

    Add DI by interface instead of class

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarServiceImp;
+import web.service.CarService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +15,7 @@ import java.util.List;
 public class CarsController {
 
     @Autowired
-    private CarServiceImp serviceImp;
+    private CarService carService;
 
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer count,
@@ -34,7 +34,7 @@ public class CarsController {
 
         // передача модели для обработки списка
         // получение итогового списка через сервис/дао
-        List<Car> carsResult = serviceImp.getCars(model);
+        List<Car> carsResult = carService.getCars(model);
 
         // упаковка в модель итогового списка
         model.addAttribute("carsResult", carsResult);
