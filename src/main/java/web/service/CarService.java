0c845320dd488916aa206a2f5c167c6c2cf5dea3
kunjut <v.dimnich@gmail.com>
2021-03-09 14:18:25 +0300

    Method parameter change

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,11 +1,10 @@
 package web.service;
 
-import org.springframework.ui.ModelMap;
 import web.model.Car;
 
 import java.util.List;
 
 public interface CarService {
 
-    List<Car> getCars(ModelMap model);
+    List<Car> getCars(Integer count);
 }
