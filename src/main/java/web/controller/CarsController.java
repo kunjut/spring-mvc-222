00123199bfb37e8a0f79337f328afc75fb706afe
kunjut <v.dimnich@gmail.com>
2021-03-09 14:06:02 +0300

    transferring storage of machines to a service method

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -5,11 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarsController {
@@ -21,23 +17,9 @@ public class CarsController {
     public String printCars(@RequestParam(value = "count", required = false) Integer count,
                             ModelMap model) {
 
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("BMW", "X1", 2016));
-        cars.add(new Car("BMW", "X3", 2017));
-        cars.add(new Car("BMW", "X4", 2018));
-        cars.add(new Car("BMW", "X5", 2019));
-        cars.add(new Car("BMW", "X6", 2020));
-
-        // упакова в модель полученного параметра и списка
-        model.addAttribute("count", count);
-        model.addAttribute("cars", cars);
-
-        // передача модели для обработки списка
-        // получение итогового списка через сервис/дао
-        List<Car> carsResult = carService.getCars(model);
-
-        // упаковка в модель итогового списка
-        model.addAttribute("carsResult", carsResult);
+        // получение итогового списка через метод сервиса
+        // и упаковка его в модель
+        model.addAttribute("carsResult", carService.getCars(count));
 
         // возврат вьюхи
         return "cars";
